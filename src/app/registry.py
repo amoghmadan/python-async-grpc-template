de9f607306c3e6fa774e2314e606d329a7cda989
Amogh Madan <amoghmadaan@gmail.com>
2025-12-09 22:35:57 +0530

    Move from function to dict.

--- src/app/registry.py
+++ src/app/registry.py
@@ -1,16 +1,6 @@
-from grpc.aio import Server
-
 from app.protobuf.healthcheck import healthcheck_pb2_grpc
 from app.services import HealthCheckService
 
-
-def register_services(server: Server) -> Server:
-    """
-    Register gRPC services with the application.
-    :param server: Server, the grpc.aio.server instance.
-    :return: Server
-    """
-    healthcheck_pb2_grpc.add_HealthCheckServiceServicer_to_server(
-        HealthCheckService(), server
-    )
-    return server
+register = {
+    HealthCheckService: healthcheck_pb2_grpc.add_HealthCheckServiceServicer_to_server,
+}
